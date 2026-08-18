24c15a9031d0c2f93060f843036508f93136cba8
MatJasin <>
2026-08-18 09:28:34 +0000

    Challenge: Combining Table Results

---
+++ nested_subqueries/challenge_combining_table_results/query.sql
@@ -0,0 +1,10 @@
+SELECT *
+FROM (
+  SELECT employee_id as id, first_name, last_name
+  FROM employees
+    UNION
+    SELECT contractor_id as id, first_name, last_name
+    FROM contractors
+  ) AS combined
+-- Don't forget to write the WHERE clause here
+WHERE id % 2 = 0;
