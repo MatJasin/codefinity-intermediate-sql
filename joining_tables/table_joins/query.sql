a7dac6ff9c27c113381176da5d810c42da9dd729
MatJasin <>
2026-08-19 02:27:41 +0000

    Table Joins

---
+++ joining_tables/table_joins/query.sql
@@ -0,0 +1,3 @@
+SELECT courses.course_id, courses.course_name, courses.description, enrollments.student_name, enrollments.enrollment_date
+FROM courses
+LEFT JOIN enrollments ON enrollments.course_id = courses.course_id
